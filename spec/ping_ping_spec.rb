d881b3ba8105983d2015fc1046eed85934d27453
Todd Pangilinan <pang.todd@gmail.com>
2022-04-12 23:14:28 -0700

    start spec 2, for sort function. WIP, going to bed.

--- spec/ping_ping_spec.rb
+++ spec/ping_ping_spec.rb
@@ -2,6 +2,7 @@ require('rspec')
 require('ping_pong')
 
 describe('#numbers_loop') do
-   it("returns an array of ascending numbers up to the number entered") do
-    expect(numbers_loop(2)).to(eq([1,2]))
+  it("returns an array of ascending numbers up to the number entered") do
+    expect(numbers_loop(2)).to eq([1,2])
   end
+end
